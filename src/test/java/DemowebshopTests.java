747cbcf28eafd1586e007e84e8992a82cefde419
svasenkov <aanher@gmail.com>
2023-01-20 22:11:41 +0300

    added DemoWebshopTests

---
+++ src/test/java/DemowebshopTests.java
@@ -0,0 +1,61 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.given;
+
+public class DemowebshopTests {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://demowebshop.tricentis.com";
+    }
+
+    @Test
+    void addToCartTest() {
+        String cookieValue = "C3B23684816523C3E10934D04BB7269BD98869D9B42CE39E42379171707" +
+                "1721A102B8200F60C64202EFF08A8BB31019B281869DCC0A013C1BDDB8A2C040B083CB7EE18743398AB5F22BC4F575" +
+                "96FA47732C6ED3EF4505FAC4C44E1324BC72D5D6C434630381EAAD9EF1A5CC8316CAA105B3A34D3709FBA22C343FD" +
+                "CB8F4546262AC16884DD69B73F0ECE3F16DFCBAE2A;",
+                body = "product_attribute_72_5_18=65" +
+                        "&product_attribute_72_6_19=54" +
+                        "&product_attribute_72_3_20=57" +
+                        "&product_attribute_72_8_30=94" +
+                        "&addtocart_72.EnteredQuantity=3";
+
+        given()
+//                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("NOPCOMMERCE.AUTH", cookieValue) // todo move to file
+                .body(body)
+                .when()
+                .post("/addproducttocart/details/72/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"));
+    }
+
+    @Test
+    void addToCartAnonymTest() {
+        String body = "product_attribute_72_5_18=65" +
+                        "&product_attribute_72_6_19=54" +
+                        "&product_attribute_72_3_20=57" +
+                        "&product_attribute_72_8_30=94" +
+                        "&addtocart_72.EnteredQuantity=3";
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body(body)
+                .when()
+                .post("/addproducttocart/details/72/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .body("updatetopcartsectionhtml", is("(3)"));
+    }
+}
